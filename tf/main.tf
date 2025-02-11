1fef27a3aeab07e04cc8c9fc637d97e5ef6c8b21
Juraj Kazda <juraj.kazda@oracle.com>
2025-02-11 15:02:23 +0100

    First fully functional version

--- tf/main.tf
+++ tf/main.tf
@@ -6,7 +6,7 @@ locals {
   compartment_ocid = "ocid1.compartment.oc1..aaaaaaaasluc2z5magbxoa3ozfptgz5bwglk7jee4mbecc2il6hrnh7tq6ra"
 
   # name
-  display_name = "vm-osmh"
+  display_name = "vm-fn-secrets-3"
 
   # base AF shape
   shape = "VM.Standard.E4.Flex"
@@ -20,6 +20,16 @@ locals {
 
   # sn-dev-public
   subnet_id = "ocid1.subnet.oc1.uk-london-1.aaaaaaaaeggtoa3vvcdsjjuqzsp5dtjuyfwbqmsms36okajof3c3nu7ow4xq"
+
+  fn_input = {
+    "keyPairName": "vm-fn-secrets-3",
+    "compartmentId": "ocid1.compartment.oc1..aaaaaaaappd5di5sx5kr4s7gcdkc7mlbicrn3ydkkjhjc4zyhcc4u3hdqxqa",
+    "vaultId": "ocid1.vault.oc1.uk-london-1.ertrmuzkaagpk.abwgiljttvlqsststysrk45vdkbtg4kgby4rtwnu4lfzz5eqkbpdgd5jw3cq",
+    "keyId": "ocid1.key.oc1.uk-london-1.ertrmuzkaagpk.abwgiljrpqazigekhz47dgwhbzvtsi6j3lazo6secuuqh622w5n6h7jfk4aa",
+    "description": "vm-fn-secrets-1 keyPair desc."
+  }
+
+  public_key = jsondecode(oci_functions_invoke_function.invoke_example.content).publicKeySecretValue
 }
 
 # data "oci_functions_function" "example_function" {
@@ -29,13 +39,29 @@ locals {
 
 resource "oci_functions_invoke_function" "invoke_example" {
   function_id = "ocid1.fnfunc.oc1.uk-london-1.aaaaaaaah5jvzgvwg2uvcjxwjkwlrhancjya4ewjirwk6vx3b3sno3es7wwa"
+  invoke_function_body = jsonencode(local.fn_input)
+#   function_id = "ocid1.fnfunc.oc1.uk-london-1.aaaaaaaah5jvzgvwg2uvcjxwjkwlrhancjya4ewjirwk6vx3b3sno3es7wwa"
 }
 
 output "function_invoke_response" {
   value = oci_functions_invoke_function.invoke_example.content
 }
 
-/*
+output "function_invoke_response_private_key_secret_ocid" {
+  value = jsondecode(oci_functions_invoke_function.invoke_example.content).privateKeySecretOcid
+}
+
+
+# Data source to retrieve the SSH private key from the Vault
+data "oci_secrets_secretbundle" "ssh_private_key" {
+  secret_id = jsondecode(oci_functions_invoke_function.invoke_example.content).privateKeySecretOcid
+}
+
+output "private_key" {
+  value = base64decode(data.oci_secrets_secretbundle.ssh_private_key.secret_bundle_content.0.content)
+}
+
+
 resource "oci_core_instance" "my_compute" {
   availability_domain = local.availability_domain
   compartment_id      = local.compartment_ocid
@@ -46,7 +72,8 @@ resource "oci_core_instance" "my_compute" {
   }
 
   metadata = {
-    ssh_authorized_keys = local.ssh_authorized_keys
+    ssh_authorized_keys = local.public_key
+#     ssh_authorized_keys = base64decode(data.oci_secrets_secretbundle.ssh_private_key.secret_bundle_content.0.content) #local.ssh_authorized_keys
   }
 
   shape_config {
@@ -60,39 +87,4 @@ resource "oci_core_instance" "my_compute" {
   }
 
   display_name = local.display_name
-
-  agent_config {
-    is_management_disabled = false
-    is_monitoring_disabled = false
-
-    plugins_config {
-      name          = "OS Management Hub Agent"
-      desired_state = "ENABLED"
-    }
-
-    plugins_config {
-      name          = "OS Management Service Agent"
-      desired_state = "DISABLED"
-    }
-
-    plugins_config {
-      name          = "Management Agent"
-      desired_state = "ENABLED"
-    }
-  }
 }
-
-
-output "my_compute_osmh" {
-  value = data.oci_os_management_hub_managed_instance.my_compute
-}
-# resource "oci_os_management_hub_managed_instance" "test_managed_instance" {
-#   #Required
-#   managed_instance_id = oci_core_instance.my_compute.id
-# }
-
-output "profile_id" {
-  value = data.oci_os_management_hub_profiles.osmhp_ol8_amd64
-}
-
-*/
